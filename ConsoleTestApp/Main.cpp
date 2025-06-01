4c1def9d51d87d46a79b5fef4e540939590187fe
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-01 18:08:05 +0100

    Test console app for easy debug printing

---
+++ ConsoleTestApp/Main.cpp
@@ -0,0 +1,13 @@
+#pragma comment(lib, "Common_Debug")
+
+#include <Common/Math/Mat3x3.h>
+#include <Common/Math/Mat4x4.h>
+#include <Common/Math/Vector4.h>
+#include <Common/Math/Vector3.h>
+#include <Common/Math/RotationUnits.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
