987b2d8aa1e79387585c290f6550fbba67b48ab2
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-02 14:47:39 +0100

    Vertex constructors and assignment

--- Common/Rendering/Vertex.h
+++ Common/Rendering/Vertex.h
@@ -4,8 +4,16 @@
 
 struct Vertex_Pos4_fCol4
 {
+public:
 	Vector4f Position;
 	RGBAColor_f32 Color;
+
+public:
+	Vertex_Pos4_fCol4() = default;
+	Vertex_Pos4_fCol4(const Vertex_Pos4_fCol4&) = default;
+	Vertex_Pos4_fCol4(const Vector4f& p, const RGBAColor_f32& c) : Position{p} , Color{c} { }
+
+	Vertex_Pos4_fCol4& operator=(const Vertex_Pos4_fCol4&) = default;
 };
 
 // Define the basic vertex type
