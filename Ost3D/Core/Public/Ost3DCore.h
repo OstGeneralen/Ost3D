3c4d1f6e2a26df530a576afd2178973a46d578bd
OstGeneralen <kasper.esbjornsson@gmail.com>
2025-06-07 19:59:27 +0100

    Make Ost3DCore.h which pulls in most common tools from Core

---
+++ Ost3D/Core/Public/Ost3DCore.h
@@ -0,0 +1,19 @@
+// Ost3D::Core - Kasper "OstGeneralen" Esbjornsson - 2025
+
+// ------------------------------------------------------------
+// Full Size utility header for common includes from the core
+// library.
+// ------------------------------------------------------------
+
+#pragma once
+
+#include <Ost3D/Core/Public/Math.h>
+
+#include <Ost3D/Core/Public/Utility/FlagEnum.h>
+#include <Ost3D/Core/Public/Utility/Events.h>
+#include <Ost3D/Core/Public/Utility/IDType.h>
+#include <Ost3D/Core/Public/Utility/Memory/MemoryOps.h>
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+// ------------------------------------------------------------
